358af852f08826a001cbb62f6700d3b449920fa4
Huma Prathama <huma.elektro@gmail.com>
2017-07-31 15:11:42 +0800

    cleanup

--- humadev-angular-theme/index.ts
+++
@@ -1,6 +0,0 @@
-export {HdLayoutModule} from "./lib/hd-layout/hd-layout.module";
-export {HdLoginModule} from "./lib/login/hd-login.module";
-export {HdContextMenuModule} from "./lib/context-menu/hd-context-menu.module";
-export { ComponentInjectionService } from "./lib/services/component-injection.service";
-export { TableAdapter } from "./lib/class/table-adapter";
-export {FabSpeedDialModule} from "./lib/fab-speed-dial/fab-speed-dial.module";
