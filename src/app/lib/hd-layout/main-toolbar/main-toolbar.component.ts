b14dd97a5e478dbc48a2f4e2c92d5bbea8ef9340
huma prathama <humaprathama@humas-iMac.local>
2017-09-08 18:41:04 +0800

    add asset metronic

--- src/app/lib/hd-layout/main-toolbar/main-toolbar.component.ts
+++ src/app/lib/hd-layout/main-toolbar/main-toolbar.component.ts
@@ -3,36 +3,7 @@ import { MenuService } from "../../services/menu.service";
 
 @Component({
   selector: 'hd-main-toolbar',
-  template: `
-      <md-toolbar class="hd-main-toolbar mat-elevation-z8" color="primary">
-            <span class="hd-sidenav-toggle" *ngIf="showSidenavToggle" (click)="onSidenavToggle()"><md-icon>menu</md-icon></span>
-            <img class="hd-title-bar" [src]="titleImg" *ngIf="titleImg; else titleTextEl">
-            <ng-template #titleTextEl><span class="hd-title-bar">{{titleText}}</span></ng-template>
-            <md-select class="fill-space" (change)="onChange($event)" [(ngModel)]="active">
-                <ng-template ngFor let-menu [ngForOf]="startMenus">
-                    <md-option *ngIf="menu.data.isMenu" [value]="menu.path">
-                        {{ menu.data.title }}
-                    </md-option>
-                </ng-template>
-            </md-select>
-            <md-menu #menu="mdMenu" class="hd-profile-menu" yPosition="below" xPosition="before" [overlapTrigger]="false">
-                  <button md-menu-item (click)="onLogout()">
-                        <md-icon>dashboard</md-icon>
-                        <span>Menu 2</span>
-                  </button>
-            </md-menu>
-            <span class="hd-menu hd-right-menu" *ngIf="notification"><md-icon>notifications_none</md-icon></span>
-            <button md-button [mdMenuTriggerFor]="menu" #profileMenu class="hd-menu hd-right-menu" *ngIf="profile">
-                  <md-icon>face</md-icon> Admin
-            </button>
-            <md-menu #menu="mdMenu" class="hd-profile-menu" yPosition="below" xPosition="before" [overlapTrigger]="false">
-                  <button md-menu-item (click)="onLogout()">
-                        <md-icon>power_settings_new</md-icon>
-                        <span>Logout</span>
-                  </button>
-            </md-menu>
-      </md-toolbar>
-  `,
+  templateUrl: 'main-toolbar.component.html',
   styles: [`
       .hd-main-toolbar{
             position: fixed;
