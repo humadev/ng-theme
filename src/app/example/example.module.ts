b642553a856264f95b59ed1b47ac2daa40bca9a5
huma prathama <humaprathama@humas-iMac.local>
2017-10-05 10:30:37 +0800

    change example

--- src/app/example/example.module.ts
+++ src/app/example/example.module.ts
@@ -1,19 +1,19 @@
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { NgModule } from '@angular/core';
-import { CommonModule } from "@angular/common";
-import { SharedModule } from "app/shared.module";
+import { CommonModule } from '@angular/common';
+import { SharedModule } from 'app/shared.module';
 import { RouterModule, Routes } from '@angular/router';
-import { ReactiveFormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from '@angular/forms';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ModalComponent } from './modal/modal.component';
 import { ExampleComponent } from './example.component';
-import { MdTableModule, MdPaginatorModule, MdSortModule } from "@angular/material";
-import { CdkTableModule } from "@angular/cdk/table";
+import { MdTableModule, MdPaginatorModule, MdSortModule } from '@angular/material';
+import { CdkTableModule } from '@angular/cdk/table';
 
 
 import { ApolloClient, createNetworkInterface } from 'apollo-client';
-import { ApolloModule } from "apollo-angular";
-import { DataService } from "app/example/data.service";
+import { ApolloModule } from 'apollo-angular';
+import { DataService } from 'app/example/data.service';
 
 const networkInterface = createNetworkInterface({ uri: 'https://sandbox.undiknas.ac.id/graphql' });
 
