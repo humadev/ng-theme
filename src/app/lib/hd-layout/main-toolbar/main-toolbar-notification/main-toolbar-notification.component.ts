3ff9e82d125bd46277d4ba3789b9adcfc2da7ffa
huma prathama <humaprathama@humas-iMac.local>
2017-10-16 18:46:36 +0800

    testing

--- src/app/lib/hd-layout/main-toolbar/main-toolbar-notification/main-toolbar-notification.component.ts
+++ src/app/lib/hd-layout/main-toolbar/main-toolbar-notification/main-toolbar-notification.component.ts
@@ -1,9 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'hd-main-toolbar-notification',
+  selector: '[hd-main-toolbar-notification]',
   templateUrl: './main-toolbar-notification.component.html',
-  styleUrls: ['./main-toolbar-notification.component.scss']
+  styleUrls: ['./main-toolbar-notification.component.scss'],
+  styles: [`
+    :host{
+        padding: 0 12px;
+        height: 100%;
+        display: inline-block;
+        vertical-align: middle;
+    }
+  `]
 })
 export class MainToolbarNotificationComponent implements OnInit {
 
