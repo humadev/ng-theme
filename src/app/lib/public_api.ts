723e8ac91ad9db917936da8ae6bdfda483f59c14
Huma Prathama <huma.elektro@gmail.com>
2017-11-27 14:23:08 +0800

    mispell export

--- src/app/lib/public_api.ts
+++ src/app/lib/public_api.ts
@@ -1,4 +1,4 @@
-import { HdUploadModule } from './hd-upload/hd-upload.module';
+export { HdUploadModule } from './hd-upload/hd-upload.module';
 export { HdLayoutModule } from './hd-layout/hd-layout.module';
 export { HdLoginModule } from './login/hd-login.module';
 export { HdContextMenuModule } from './context-menu/hd-context-menu.module';
