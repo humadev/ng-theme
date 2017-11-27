69800f6044f6570dfebfb68957286cc52996b686
Huma Prathama <huma.elektro@gmail.com>
2017-11-27 13:59:18 +0800

    export upload module into public

--- src/app/lib/public_api.ts
+++ src/app/lib/public_api.ts
@@ -1,4 +1,5 @@
 export { HdLayoutModule } from './hd-layout/hd-layout.module';
+export { HdUploadModule } from './hd-upload/hd-upload.module';
 export { HdLoginModule } from './login/hd-login.module';
 export { HdContextMenuModule } from './context-menu/hd-context-menu.module';
 export { TableAdapter } from './class/table-adapter';
