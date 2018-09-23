4e50a9a065febd730b5a2038173758d1f8d0e4ec
huma prathama <huma.elektro@gmail.com>
2018-09-23 23:05:55 +0800

    add me to helper

--- projects/humadev/ng-theme/src/lib/context-menu/context-menu.ts
+++ projects/humadev/ng-theme/src/lib/context-menu/context-menu.ts
@@ -1,14 +1,15 @@
 class GroupPermission {
   permissions: [number];
-  groups:[number];
+  groups: [number];
 }
 
 export class ContextMenu {
-    icon: String;
-    title: String;
-    callback: Function;
-    groupPermission?: GroupPermission;
-    display?: Boolean | Function;
-    displayCallback?: Boolean = true;
-    children?: [ContextMenu];
+  icon: String;
+  title: String;
+  callback: Function;
+  groupPermission?: GroupPermission;
+  display?: Boolean | Function;
+  displayCallback?: Boolean = true;
+  children?: [ContextMenu];
+  status?: [number];
 }
