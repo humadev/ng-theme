ef648ef24f1d251f0c71af5c92c63be51a1e80fb
huma prathama <huma.elektro@gmail.com>
2020-06-04 22:00:31 +0800

    remove publish

--- publish/services/layout.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs/Rx';
-
-@Injectable()
-export class LayoutService {
-
-      pageTitle = new BehaviorSubject<string>('');
-      pageProgressBar= true;
-}
